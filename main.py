b085fe79ba75abd1db30113754433a6f511b0674
meektion <168012408+meektion@users.noreply.github.com>
2025-01-01 12:50:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## [Gitblog](https://meektion.github.io/)
-My personal blog([About Me](https://meektion.github.io//issues/1)) using issues and GitHub Actions (随意转载，无需署名)
+My personal blog([About Me](https://meektion.github.io/issues/1)) using issues and GitHub Actions (随意转载，无需署名)
 ![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/main/feed.xml)
 """
